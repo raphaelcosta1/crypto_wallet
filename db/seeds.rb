7a112c887dcec74636f883bb38462d5289c48632
raphael costa <raphaeljsc@gmail.com>
2021-09-14 11:08:38 -0300

    seedstest

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Coin.create!(
+    description: "Bitcoin",
+    acronym: "BTC",
+    url_imagem: "https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png" 
+)
+
+Coin.create!(
+    description: "Ethereum",
+    acronym: "ETH",
+    url_imagem: "https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png"
+)
+
+Coin.create!(
+  description: "Dash",
+  acronym: "DSH",
+  url_imagem: "https://w7.pngwing.com/pngs/37/123/png-transparent-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text.png" 
+)
