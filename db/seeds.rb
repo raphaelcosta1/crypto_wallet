a19c8d200756dfddcd48179659c912c3ab3d8ea7
raphael costa <raphaeljsc@gmail.com>
2021-09-14 15:31:36 -0300

    coins.each

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,20 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Coin.create!(
-  [
-    {
-      description: "Bitcoin",
-      acronym: "BTC",
-      url_image:  "https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png" 
-    },
-    {
-      description: "Ethereum",
-      acronym: "ETH",
-      url_image: "https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png"
-    },
-    {
-      description: "Dash",
-      acronym: "DSH",
-      url_image: "https://w7.pngwing.com/pngs/37/123/png-transparent-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text.png" 
-    }
+
+
+Coin.find_or_create_by!(
+
+
+coins = [
+          {
+            description: "Bitcoin",
+            acronym: "BTC",
+            url_image:  "https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png" 
+          },
+          {
+            description: "Ethereum",
+            acronym: "ETH",
+            url_image: "https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png"
+          },
+          {
+            description: "Dash",
+            acronym: "DSH",
+            url_image: "https://w7.pngwing.com/pngs/37/123/png-transparent-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text.png" 
+          }
+        ]
+
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
