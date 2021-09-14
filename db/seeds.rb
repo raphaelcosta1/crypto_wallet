184aafadfeb413adf5bb03b260e9a0a7480c1349
raphael costa <raphaeljsc@gmail.com>
2021-09-14 11:29:14 -0300

    doingtasks

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,22 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-
 Coin.create!(
     description: "Bitcoin",
-    acronym: "BTC",
-    url_imagem: "https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png" 
+    acronyn: "BTC",
+    url_image:  "https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png" 
 )
 
 Coin.create!(
     description: "Ethereum",
-    acronym: "ETH",
-    url_imagem: "https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png"
+    acronyn: "ETH",
+    url_image: "https://w7.pngwing.com/pngs/368/176/png-transparent-ethereum-cryptocurrency-blockchain-bitcoin-logo-bitcoin-angle-triangle-logo-thumbnail.png"
 )
 
 Coin.create!(
   description: "Dash",
-  acronym: "DSH",
-  url_imagem: "https://w7.pngwing.com/pngs/37/123/png-transparent-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text.png" 
+  acronyn: "DSH",
+  url_image: "https://w7.pngwing.com/pngs/37/123/png-transparent-dash-bitcoin-cryptocurrency-digital-currency-logo-bitcoin-blue-angle-text.png" 
 )
